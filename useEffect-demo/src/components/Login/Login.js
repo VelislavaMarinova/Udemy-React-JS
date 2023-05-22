6d2323896635564dfba47e463a33e5c183524ef1
Vel MAr <velmar.annabell@gmail.com>
2023-05-22 11:08:53 +0300

    Clean up function and how it works

--- useEffect-demo/src/components/Login/Login.js
+++ useEffect-demo/src/components/Login/Login.js
@@ -12,15 +12,25 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  }, [enteredEmail,enteredPassword]);
+    const identifier = setTimeout(() => {
+
+      console.log('Check validity');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+      
+    }, 2000)
+    
+    return () => {
+      console.log("CleanUp");
+      clearTimeout(identifier);
+      
+    }//cleanup function
+
+  }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-
   };
 
   const passwordChangeHandler = (event) => {
